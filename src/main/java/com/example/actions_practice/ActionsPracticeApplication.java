22795d60c7c7122ffe63a4383afd427b8d3b5e5f
soyeong Lee <gaya1273@naver.com>
2025-11-24 21:44:59 +0900

    chore: initialize Spring Boot Project
    - create project using Spring Initializer

---
+++ src/main/java/com/example/actions_practice/ActionsPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.actions_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActionsPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActionsPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/actions_practice/ActionsPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.actions_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActionsPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
